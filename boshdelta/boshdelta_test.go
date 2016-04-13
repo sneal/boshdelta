2f7a99af333981857e8021fcdf714e1503bbbb84
Shawn Neal <sneal@sneal.net>
2016-04-13 18:01:40 -0400

    Add stubbed out Ginkgo tests

---
+++ boshdelta/boshdelta_test.go
@@ -0,0 +1,12 @@
+package boshdelta_test
+
+import (
+	. "github.com/sneal/bosh-delta/boshdelta"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Boshdelta", func() {
+
+})
