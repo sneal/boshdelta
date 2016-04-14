a17dc7e9dab6a9780761c5294192d982b0f52291
Shawn Neal <sneal@sneal.net>
2016-04-14 11:44:55 -0400

    Add initial release tgz parsing

--- boshdelta/boshdelta_test.go
+++ boshdelta/boshdelta_test.go
@@ -1,6 +1,9 @@
 package boshdelta_test
 
 import (
+	"os"
+	"path/filepath"
+
 	. "github.com/sneal/bosh-delta/boshdelta"
 
 	. "github.com/onsi/ginkgo"
@@ -8,5 +11,51 @@ import (
 )
 
 var _ = Describe("Boshdelta", func() {
+	Context("Redis BOSH Release 12", func() {
+		var (
+			releasePath string
+			release     *Release
+		)
 
+		BeforeEach(func() {
+			dir, err := os.Getwd()
+			Expect(err).NotTo(HaveOccurred())
+			releasePath = filepath.Join(dir, "../fixtures/redis-boshrelease-12.tgz")
+			release, err = NewRelease(releasePath)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("can be loaded", func() {
+			Expect(release).NotTo(BeNil())
+		})
+		It("has the correct path", func() {
+			Expect(release.Path).To(Equal(releasePath))
+		})
+		It("has uncommitted changes", func() {
+			Expect(release.UncommittedChanges).To(BeTrue())
+		})
+		It("is version 12", func() {
+			Expect(release.Version).To(Equal("12"))
+		})
+		It("is named redis", func() {
+			Expect(release.Name).To(Equal("redis"))
+		})
+		It("contains 2 jobs", func() {
+			Expect(release.Jobs).To(HaveLen(2))
+		})
+		It("contains the acceptance-tests job", func() {
+			job := release.Jobs[0]
+			Expect(job.Name).To(Equal("acceptance-tests"))
+			Expect(job.Version).To(Equal("d1b927ac3839a52f7caa29ac80878d8625fb4bcc"))
+			Expect(job.Sha1).To(Equal("dee26bdad425c0276df120c922551c994777cb42"))
+			Expect(job.Fingerprint).To(Equal("d1b927ac3839a52f7caa29ac80878d8625fb4bcc"))
+		})
+		It("contains the redis job", func() {
+			job := release.Jobs[1]
+			Expect(job.Name).To(Equal("redis"))
+			Expect(job.Version).To(Equal("a52b9ad8bf105901dbd55e6dfb3af359b24a2d14"))
+			Expect(job.Sha1).To(Equal("fbb1fe127a25429c84a120c8f676c3e7f26ff051"))
+			Expect(job.Fingerprint).To(Equal("a52b9ad8bf105901dbd55e6dfb3af359b24a2d14"))
+		})
+	})
 })
--- boshdelta/release.go
+++ boshdelta/release.go
@@ -1,21 +1,74 @@
 package boshdelta
 
-type release struct {
-	path string
-	jobs []job
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cloudfoundry-incubator/candiedyaml"
+)
+
+const releaseManifestFileName string = "release.MF"
+
+// Release is a BOSH release
+type Release struct {
+	Path               string
+	Jobs               []Job  `yaml:"jobs"`
+	Name               string `yaml:"name"`
+	CommitHash         string `yaml:"commit_hash"`
+	UncommittedChanges bool   `yaml:"uncommitted_changes"`
+	Version            string `yaml:"version"`
 }
 
-type job struct {
+// Job is a job in a BOSH release
+type Job struct {
+	Name        string `yaml:"name"`
+	Version     string `yaml:"version"`
+	Sha1        string `yaml:"sha1"`
+	Fingerprint string `yaml:"fingerprint"`
 }
 
-func newRelease(releasePath string) *release {
-	r := &release{
-		path: releasePath,
+// NewRelease creates a release reading in the BOSH release metadata
+func NewRelease(releasePath string) (*Release, error) {
+	r := &Release{
+		Path: releasePath,
 	}
-	r.readManifest()
-	return r
+	err := r.readManifest()
+	return r, err
 }
 
-func (*release) readManifest() {
+func (r *Release) readManifest() (err error) {
+	f, err := os.Open(r.Path)
+	if err != nil {
+		return err
+	}
+	defer func() { err = f.Close() }()
+	gzf, err := gzip.NewReader(f)
+	if err != nil {
+		return err
+	}
+	tarReader := tar.NewReader(gzf)
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
 
+		info := header.FileInfo()
+		if !info.IsDir() && info.Name() == releaseManifestFileName {
+			decoder := candiedyaml.NewDecoder(tarReader)
+			err = decoder.Decode(&r)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			break
+		}
+	}
+
+	return nil
 }
